deea9824521ec7b2805a08cc76066011d3403890
Chris Eady <chris.eady1@gmail.com>
2019-02-18 15:27:37 -0600

    Add gatsby specific env variables for sanity config

--- src/lib/image-url.js
+++ src/lib/image-url.js
@@ -1,7 +1,11 @@
 import imageUrlBuilder from '@sanity/image-url'
-import { sanityConfig } from '../../gatsby-config'
 
-const builder = imageUrlBuilder(sanityConfig.api)
+const sanityConfig = {
+  projectId: process.env.GATSBY_SANITY_PROJECT_ID,
+  dataset: process.env.GATSBY_SANITY_DATASET,
+}
+
+const builder = imageUrlBuilder(sanityConfig)
 
 export function imageUrlFor(source) {
   return builder.image(source)
